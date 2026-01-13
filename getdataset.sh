d1551612cbc8418eb542c8f0e86b462035302aa7
Claudio Desideri <happy.snizzo@gmail.com>
2026-01-13 15:31:44 +0100

    Small fix on dataset downloader

--- getdataset.sh
+++ getdataset.sh
@@ -3,6 +3,8 @@
 wget https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/fwhytt5mzd-2.zip
 unzip Axial\ CT\ Imaging\ Dataset\ for\ AI-Powered\ Kidney\ Stone\ Detection\ A\ Resource\ for\ Deep\ Learning\ Research.zip
 unrar x Axial\ CT\ Imaging\ Dataset\ for\ AI-Powered\ Kidney\ Stone\ Detection\ A\ Resource\ for\ Deep\ Learning\ Research/Kindey\ Stone\ Dataset.rar
+mkdir -p augmented
+mkdir -p traindata
 mv Kindey\ Stone\ Dataset/Augmented augmented
 mv Kindey\ Stone\ Dataset/Original traindata
 
